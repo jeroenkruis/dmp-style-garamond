835f897aa99f28ad58d6fabd920f77b7f5ee5afc
Jeroen Kruis <kruisjeroen@gmail.com>
2015-03-20 12:14:31 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# dmp-garamond-styling
+
+> Garamond styling for documents written in Documark.
+
+This plugin adds [Garamond](http://en.wikipedia.org/wiki/Garamond) styling to your documents written in [Documark](http://documark.io/).
+
+## Notes
+This plugin is still under development and document breaking changes may occur.
---
+++ index.js
@@ -0,0 +1,14 @@
+var resolve = require('path').resolve;
+var file    = resolve(__dirname, 'assets/css', 'style.css');
+
+module.exports = function dmpStyleBasic ($, document, done) {
+    var $container = $('head');
+
+    if ( ! $container.length) {
+            $container = $.root();
+        }
+
+    $container.prepend('<link rel="stylesheet" type="text/css" href="file://' + file + '"/>');
+
+    done();
+};
