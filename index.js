f7a0f2cfce53782a5271377de38cea8be3e1e694
Jeroen Kruis <kruisjeroen@gmail.com>
2015-05-06 12:22:20 +0200

    Added tab indenting and removed colon in stylesheet

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var resolve = require('path').resolve;
-var file    = resolve(__dirname, 'assets/css', 'style.css');
+var file = resolve(__dirname, 'assets/css', 'style.css');
 
 module.exports = function dmpStyleBasic ($, document, done) {
-    var $container = $('head');
+	var $container = $('head');
 
-    if ( ! $container.length) {
-            $container = $.root();
-        }
+	if ( ! $container.length) {
+		$container = $.root();
+	}
 
-    $container.prepend('<link rel="stylesheet" type="text/css" href="file://' + file + '"/>');
+	$container.prepend('<link rel="stylesheet" type="text/css" href="file://' + file + '"/>');
 
-    done();
+	done();
 };
